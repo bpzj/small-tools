eae3002ee0d043956d9c4c3d6356e27d0d064d8e
bpzj <bpzj93@gmail.com>
2019-03-21 00:25:34 +0800

    update

--- python-script/stock/choose.py
+++ python-script/stock/choose.py
@@ -4,10 +4,18 @@ stocks = tushare.get_stock_basics()
 
 # 过滤城市
 cities = ['北京', '上海', '深圳', '杭州']
-stocks = stocks.loc[stocks.area.isin(cities)]
+filter_city = stocks.loc[stocks.area.isin(cities)]
 # codes = stocks.query('area == "上海"')
 
+# 上市日期 15年之后
+filter_year = filter_city.loc[filter_city.timeToMarket >= 20150101]
 
-print(stocks)
+# 行业
+
+# 流通值范围
+
+# 30日涨幅
+
+print(filter_year)
 
 
