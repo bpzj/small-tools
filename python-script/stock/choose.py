007feb58f0200014c133739bc9a006a7f05e4107
bpzj <bpzj93@gmail.com>
2019-03-21 18:23:31 +0800

    update

--- python-script/stock/choose.py
+++ python-script/stock/choose.py
@@ -1,10 +1,12 @@
-import tushare
+import tushare as ts
 
-stocks = tushare.get_stock_basics()
+ts.set_token("14bb77ad0add9b8326b8d694a3a4b8ac6d84b48f5b297a866d98129a")
+pro = ts.pro_api()
+stocks = pro.pro.stock_company(exchange='SZSE', fields='ts_code,chairman,manager,secretary,reg_capital,setup_date,province,city')
 
 # 过滤城市
 cities = ['北京', '上海', '深圳', '杭州']
-filter_city = stocks.loc[stocks.area.isin(cities)]
+filter_city = stocks.loc[stocks.city.isin(cities)]
 # codes = stocks.query('area == "上海"')
 
 # 上市日期 15年之后
