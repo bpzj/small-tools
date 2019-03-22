98ec53809bba8d60693e49ec5235267a88423d76
bpzj <bpzj93@gmail.com>
2019-03-22 17:55:55 +0800

    update

--- python-script/trade/util.py
+++ python-script/trade/util.py
@@ -5,9 +5,10 @@ import win32con
 
 def cap_img(handle=None):
     hwnd = 0xA0b12
+    hwnd = 0x70810
     left, top, right, bot = win32gui.GetWindowRect(hwnd)
-    w = right - left
-    h = bot - top
+    w = int((right - left)/1.25)
+    h = int((bot - top)/1.25)
 
     # 返回句柄窗口的设备环境、覆盖整个窗口，包括非客户区，标题栏，菜单，边框
     hwndDC = win32gui.GetWindowDC(hwnd)
@@ -26,10 +27,10 @@ def cap_img(handle=None):
     # 截图至内存设备描述表
     img_dc = mfcDC
     mem_dc = saveDC
-    mem_dc.BitBlt((0, 0), (w, h), img_dc, (100, 100), win32con.SRCCOPY)
+    mem_dc.BitBlt((0, 0), (w, h), img_dc, (125, 125), win32con.SRCCOPY)
 
     # 将截图保存到文件中
-    saveBitMap.SaveBitmapFile(mem_dc, 'screenshot.bmp')
+    saveBitMap.SaveBitmapFile(mem_dc, 'screenshot1.bmp')
     # 内存释放
     win32gui.DeleteObject(saveBitMap.GetHandle())
     saveDC.DeleteDC()
