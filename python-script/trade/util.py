9347e213200cd4041c666ccfa28662f286e94502
bpzj <bpzj93@gmail.com>
2019-03-25 10:04:36 +0800

    update

--- python-script/trade/util.py
+++ python-script/trade/util.py
@@ -1,41 +1,62 @@
 import win32gui
 import win32ui
 import win32con
+from ctypes import windll
+from PIL import Image
 
 
 def cap_img(handle=None):
-    hwnd = 0xA0b12
-    hwnd = 0x70810
+    hwnd = 0x10A8C
+    hwnd = 0x10AA0
+    # 获取句柄窗口的大小信息
+    # 可以通过修改该位置实现自定义大小截图
     left, top, right, bot = win32gui.GetWindowRect(hwnd)
-    w = int((right - left)/1.25)
-    h = int((bot - top)/1.25)
+    w = right - left
+    h = bot - top
 
     # 返回句柄窗口的设备环境、覆盖整个窗口，包括非客户区，标题栏，菜单，边框
     hwndDC = win32gui.GetWindowDC(hwnd)
-
     # 创建设备描述表
     mfcDC = win32ui.CreateDCFromHandle(hwndDC)
-
     # 创建内存设备描述表
     saveDC = mfcDC.CreateCompatibleDC()
 
+    #
     # 创建位图对象
     saveBitMap = win32ui.CreateBitmap()
     saveBitMap.CreateCompatibleBitmap(mfcDC, w, h)
     saveDC.SelectObject(saveBitMap)
 
+    #
     # 截图至内存设备描述表
     img_dc = mfcDC
     mem_dc = saveDC
-    mem_dc.BitBlt((0, 0), (w, h), img_dc, (125, 125), win32con.SRCCOPY)
-
+    mem_dc.BitBlt((0, 0), (w, h), img_dc, (0, 0), win32con.SRCCOPY)
     # 将截图保存到文件中
-    saveBitMap.SaveBitmapFile(mem_dc, 'screenshot1.bmp')
+    saveBitMap.SaveBitmapFile(mem_dc, 'screenshot.bmp')
+
+    # 改变下行决定是否截图整个窗口，可以自己测试下
+    # result = windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), 1)
+    result = windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), 0)
+
+    # 获取位图信息
+    bmpinfo = saveBitMap.GetInfo()
+    bmpstr = saveBitMap.GetBitmapBits(True)
+    # 生成图像
+    im = Image.frombuffer('RGB',
+                          (bmpinfo['bmWidth'], bmpinfo['bmHeight']),
+                          bmpstr, 'raw', 'BGRX', 0, 1)
+
     # 内存释放
     win32gui.DeleteObject(saveBitMap.GetHandle())
     saveDC.DeleteDC()
     mfcDC.DeleteDC()
     win32gui.ReleaseDC(hwnd, hwndDC)
+
+    # 存储截图
+    if result == 1:
+        # PrintWindow Succeeded
+        im.save("test.png")
 
 
 if __name__ == '__main__':
