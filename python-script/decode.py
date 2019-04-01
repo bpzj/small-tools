2ae9ae33ec004481ade690e5d9ff2df45cdd77cb
bpzj <1521189886@qq.com>
2019-04-01 23:36:59 +0800

    update

--- python-script/decode.py
+++
@@ -1,7 +0,0 @@
-if __name__ == '__main__':
-    print("期货概念885570：" + "885570".encode("utf-8").hex())
-    print("期货概念".encode("utf-8").hex())
-    print("互联网保险".encode("utf-8").hex())
-    print("知识产权保护".encode("utf-8").hex())
-    print("区块链".encode("utf-8").hex())
-
